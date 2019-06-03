98b095ace708ab664098d584a8d101a5a364cafb
Saul <sfloresjr.0@gmail.com>
2019-06-03 02:45:42 -0700

    Histogram v1.0

---
+++ Histogram.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;	// Alternative "using std::cout" to avoid naming conflicts
+
+int main() {
+
+	int arraySize;
+	cout << "Enter the size of the array: ";
+	cin >> arraySize;
+
+	int* ageArray;
+	ageArray = new int[arraySize];
+
+	for (int i = 0; i < arraySize; i++) {
+		cout << "Enter age: ";
+		cin >> ageArray[i];	// How to avoid overflowing and limit the age range between 0 and 99
+	}
+}
